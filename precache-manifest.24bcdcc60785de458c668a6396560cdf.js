1cc9a99fad1a51084339edc79c2c18959b55464e
Lubos Jones <l.furinda@gmail.com>
2020-01-07 22:48:09 +0100

    Updates

---
+++ precache-manifest.24bcdcc60785de458c668a6396560cdf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc0834d1a8c61af1fc188ff145f6286",
+    "url": "/LubosG12/Tetris-react/index.html"
+  },
+  {
+    "revision": "73728379acac86045f47",
+    "url": "/LubosG12/Tetris-react/static/css/main.ccd07298.chunk.css"
+  },
+  {
+    "revision": "223bd5bc328da66bf175",
+    "url": "/LubosG12/Tetris-react/static/js/2.45050913.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/LubosG12/Tetris-react/static/js/2.45050913.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73728379acac86045f47",
+    "url": "/LubosG12/Tetris-react/static/js/main.7a74c05d.chunk.js"
+  },
+  {
+    "revision": "9016ce4e5c477976ff83",
+    "url": "/LubosG12/Tetris-react/static/js/runtime-main.7489fac3.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/LubosG12/Tetris-react/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "263c4490e3ab74be179ac3d127468989",
+    "url": "/LubosG12/Tetris-react/static/media/bg.263c4490.jpg"
+  }
+]);
